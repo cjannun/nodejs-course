485e079acc7a52d4b02b784359c78fdf47994c7e
cjannun <93235878+cjannun@users.noreply.github.com>
2022-11-13 18:38:09 -0500

    Chapter 14
    Completed files for Chapter 14

---
+++ 14TUT/model/Employee.js
@@ -0,0 +1,15 @@
+const mongoose = requiure('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
